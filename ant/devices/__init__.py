ad52d32d4ebe338324b572e26cd205657fcc3f75
John Whittington <git@jbrengineering.co.uk>
2022-09-30 16:46:43 +0200

    add a utility export to devices module

--- ant/devices/__init__.py
+++ ant/devices/__init__.py
@@ -4,4 +4,11 @@ from . import fitness_equipment
 from . import tire_pressure_monitor
 from . import shift
 
+device_profiles = {
+        "PowerMeter": power_meter.PowerMeter, 
+        "FitnessEquipment": fitness_equipment.FitnessEquipment,
+        "TirePressureMonitor": tire_pressure_monitor.TirePressureMonitor,
+        "Shifting": shift.Shifting,
+}
+
 __all__ = ["common", "power_meter", "fitness_equipment", "tire_pressure_monitor", "shift"]
