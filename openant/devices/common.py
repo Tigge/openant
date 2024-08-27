4f6b1d275f6470bb86f81236c6afe96fd40ab4cd
Noe Gros <i@noegros.com>
2024-08-27 17:32:07 +0100

    added extra (debug) logging

--- openant/devices/common.py
+++ openant/devices/common.py
@@ -320,6 +320,9 @@ class AntPlusDevice:
         pass
 
     def _on_data(self, data):
+
+        print(f"{self} raw data: {data}")
+        
         # extended (> 8) has the device number and id beyond page
         if len(data) > 8 and not self._attached:
             device_id = data[9] + (data[10] << 8)
